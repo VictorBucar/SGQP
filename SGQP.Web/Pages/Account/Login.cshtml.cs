1e3c476bcd453074057b5a2c5ec4b192463c10e6
raphael.coelho <rmdcoelho@gmail.com>
2018-05-28 15:08:31 -0300

    Editado layout

---
+++ SGQP.Web/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SGQP.Services.ViewModels;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace SGQP.Web.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<ActionResult> OnPost(LoginInputModel inputModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            if (!IsUserAuthenticated(inputModel.Username, inputModel.Password))
+            {
+                ModelState.AddModelError(string.Empty, "Usuário com senha inválida");
+                return Page();
+            }
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, inputModel.Username)
+
+            };
+
+            var userIdentity = new ClaimsIdentity(claims, "login");
+            ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
+
+            await HttpContext.SignInAsync(principal);
+
+            return Redirect("/");
+        }
+
+        private bool IsUserAuthenticated(string username, string password)
+        {
+            return true;
+        }
+    }
+}
--- SGQP.Web/Startup.cs
+++ SGQP.Web/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -21,6 +22,13 @@ namespace SGQP.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //adicionar o servico de cookies na aplicacao
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                    options.LoginPath = "/Account/Login"
+                );
+
             services.AddMvc();
         }
 
@@ -38,6 +46,9 @@ namespace SGQP.Web
             }
 
             app.UseStaticFiles();
+
+            //Indica que será utilizada autenticação
+            app.UseAuthentication();
 
             app.UseMvc();
         }
